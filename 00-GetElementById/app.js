56102b4c4ccc89ba1f70fec21863702759cff79e
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-04-09 07:06:32 -0500

    ADD: Add files about sample of getElementById

---
+++ 00-GetElementById/app.js
@@ -0,0 +1,4 @@
+const elemento = document.getElementById("parrafo");
+let lorem =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis nemo deserunt tempora. Nemo possimus temporibus libero provident ullam illum commodi officia voluptate, labore quibusdam vitae maxime excepturi ut atque nesciunt?";
+elemento.innerHTML = "<b>" + lorem + "</b>"; // innerHTML ademas de texto como textContent tambien agrega tags HTML
