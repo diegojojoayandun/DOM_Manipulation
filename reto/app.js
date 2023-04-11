cc32d0533d0449fc7ead8286b93a7101927c439f
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-04-11 06:26:07 -0500

    ADD: Event Listener to action buttons

--- reto/app.js
+++ reto/app.js
@@ -2,7 +2,7 @@ const fetchData = async () => {
   try {
     const res = await fetch("api.json");
     const data = await res.json();
-    renderProducts(data);
+    renderProductCards(data);
     addCarrito(data);
   } catch (error) {
     console.log(error);
@@ -11,12 +11,11 @@ const fetchData = async () => {
 
 fetchData();
 
-const renderProducts = (data) => {
+const renderProductCards = (data) => {
   const contenedorProductos = document.querySelector("#contenedor-productos");
   const template = document.querySelector("#template-productos").content;
   const fragment = document.createDocumentFragment();
   data.map((item) => {
-    //console.log(item);
     template.querySelector(".card-title").textContent = item.title;
     template.querySelector(".card-text").textContent = item.precio;
     template.querySelector("img").src = item.thumbnailUrl;
@@ -35,60 +34,69 @@ const addCarrito = (data) => {
   // EVENT DELEGATION
   arrayButtons.forEach((button) => {
     button.addEventListener("click", (event) => {
-      //console.log(button.dataset.id);
       const producto = data.find(
         (item) => item.id === parseInt(button.dataset.id)
       );
       producto.cantidad = 1;
 
       if (shoppingCar.hasOwnProperty(producto.id)) {
-        producto.cantidad++;
-        producto.total = producto.cantidad * producto.precio;
+        producto.cantidad = shoppingCar[producto.id].cantidad + 1;
+        //producto.total = producto.cantidad * producto.precio;
       }
+      producto.total = producto.cantidad * producto.precio;
 
       shoppingCar[producto.id] = { ...producto };
-      //console.log(shoppingCar);
-      renderTabla(shoppingCar);
+
+      renderTabla();
     });
   });
 };
 
 const productosTabla = document.querySelector("#items");
-const renderTabla = (data) => {
-  //   console.log(data);
+
+const renderTabla = () => {
+  items.innerHTML = "";
 
   const template = document.querySelector("#template-productos-tabla").content;
   const fragment = document.createDocumentFragment();
 
-  //   console.log(typeof data);
-  Object.values(data).forEach((item) => {
-    console.log(item.title);
+  Object.values(shoppingCar).forEach((item) => {
     template.querySelector("#td-id").textContent = item.id;
     template.querySelector("#td-title").textContent = item.title;
     template.querySelector("#td-cantidad").textContent = item.cantidad;
     template.querySelector("#td-total").textContent = item.total;
+
+    template.querySelector(".btn-success").dataset.id = item.id;
+    template.querySelector(".btn-danger").dataset.id = item.id;
+
     const clone = template.cloneNode(true);
     fragment.appendChild(clone);
   });
-  //   for (let item in data) {
-  //     template.querySelector("#td-id").textContent = item.id;
-  //     template.querySelector("#td-title").textContent = item.title;
-  //     template.querySelector("#td-cantidad").textContent = item.cantidad;
-  //     template.querySelector("#td-total").textContent = item.total;
-  //     const clone = template.cloneNode(true);
-  //     fragment.appendChild(clone);
-  //   }
-
-  //   data.forEach((item) => {
-  //     template.querySelector("#td-id").textContent = item.id;
-  //     template.querySelector("#td-title").textContent = item.title;
-  //     template.querySelector("#td-cantidad").textContent = item.cantidad;
-  //     template.querySelector("#td-total").textContent = item.total;
-  //     const clone = template.cloneNode(true);
-  //     fragment.appendChild(clone);
-  //   });
-
-  console.log(productosTabla);
 
   productosTabla.appendChild(fragment);
+
+  buttonActions();
+};
+
+const buttonActions = () => {
+  const btnAdd = document.querySelectorAll(".btn-success");
+  const btnMin = document.querySelectorAll(".btn-danger");
+
+  btnAdd.forEach((button) => {
+    button.addEventListener("click", () => {
+      const productToAdd = shoppingCar[button.dataset.id];
+      productToAdd.cantidad++;
+      shoppingCar[button.dataset.id] = { ...productToAdd };
+      renderTabla();
+    });
+  });
+
+  btnMin.forEach((button) => {
+    button.addEventListener("click", () => {
+      const productToMin = shoppingCar[button.dataset.id];
+      productToMin.cantidad--;
+      shoppingCar[button.dataset.id] = { ...productToMin };
+      renderTabla();
+    });
+  });
 };
